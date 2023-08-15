980c276635e4890a2a235864d9f81324579b98dc
Daniel Santos <daniel.santos@atomic32.com>
2023-08-15 20:25:24 -0300

    changing the favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,12 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
+import { useRouter } from "next/router"
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Tractian - Dashboard</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
